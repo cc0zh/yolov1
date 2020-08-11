3acf13d63dccbbb66011fb5f8cc64e8d7365edc6
fusimeng <fufaloveguomengkai@163.com>
2020-08-11 17:37:38 +0800

    mAP

--- eval_voc.py
+++ eval_voc.py
@@ -76,14 +76,14 @@ def voc_eval(preds, target, VOC_CLASSES=VOC_CLASSES, threshold=0.5, use_07_metri
     target {(image_id,class):[[],]}
     '''
     aps = []
-    for i,class_ in enumerate(VOC_CLASSES):
+    for i, class_ in enumerate(VOC_CLASSES):
         pred = preds[class_]  # [[image_id,confidence,x1,y1,x2,y2],...]
         if len(pred) == 0:  # 如果这个类别一个都没有检测到的异常情况
             ap = -1
-            print('---class {} ap {}---'.format(class_,ap))
+            print('---class {} ap {}---'.format(class_, ap))
             aps += [ap]
             break
-        #print(pred)
+        # print(pred)
         image_ids = [x[0] for x in pred]
         confidence = np.array([float(x[1]) for x in pred])
         BB = np.array([x[2:] for x in pred])
@@ -95,16 +95,16 @@ def voc_eval(preds, target, VOC_CLASSES=VOC_CLASSES, threshold=0.5, use_07_metri
 
         # go down dets and mark TPs and FPs
         npos = 0.
-        for (key1,key2) in target:
+        for (key1, key2) in target:
             if key2 == class_:
-                npos += len(target[(key1,key2)]) #统计这个类别的正样本，在这里统计才不会遗漏
+                npos += len(target[(key1,key2)])  # 统计这个类别的正样本，在这里统计才不会遗漏
         nd = len(image_ids)
         tp = np.zeros(nd)
         fp = np.zeros(nd)
-        for d,image_id in enumerate(image_ids):
-            bb = BB[d] #预测框
-            if (image_id,class_) in target:
-                BBGT = target[(image_id,class_)] #[[],]
+        for d, image_id in enumerate(image_ids):
+            bb = BB[d]  # 预测框
+            if (image_id, class_) in target:
+                BBGT = target[(image_id, class_)]  # [[],]
                 for bbgt in BBGT:
                     # compute overlaps
                     # intersection
@@ -118,14 +118,14 @@ def voc_eval(preds, target, VOC_CLASSES=VOC_CLASSES, threshold=0.5, use_07_metri
 
                     union = (bb[2]-bb[0]+1.)*(bb[3]-bb[1]+1.) + (bbgt[2]-bbgt[0]+1.)*(bbgt[3]-bbgt[1]+1.) - inters
                     if union == 0:
-                        print(bb,bbgt)
+                        print(bb, bbgt)
                     
                     overlaps = inters/union
                     if overlaps > threshold:
                         tp[d] = 1
-                        BBGT.remove(bbgt) #这个框已经匹配到了，不能再匹配
+                        BBGT.remove(bbgt)  # 这个框已经匹配到了，不能再匹配
                         if len(BBGT) == 0:
-                            del target[(image_id,class_)] #删除没有box的键值
+                            del target[(image_id, class_)]  # 删除没有box的键值
                         break
                 fp[d] = 1-tp[d]
             else:
@@ -134,7 +134,7 @@ def voc_eval(preds, target, VOC_CLASSES=VOC_CLASSES, threshold=0.5, use_07_metri
         tp = np.cumsum(tp)
         rec = tp/float(npos)
         prec = tp/np.maximum(tp + fp, np.finfo(np.float64).eps)
-        #print(rec,prec)
+        # print(rec,prec)
         ap = voc_ap(rec, prec, use_07_metric)
         print('---class {} ap {}---'.format(class_,ap))
         aps += [ap]
@@ -142,20 +142,34 @@ def voc_eval(preds, target, VOC_CLASSES=VOC_CLASSES, threshold=0.5, use_07_metri
 
 
 def test_eval():
-    preds = {'cat':[['image01',0.9,20,20,40,40],['image01',0.8,20,20,50,50],['image02',0.8,30,30,50,50]],'dog':[['image01',0.78,60,60,90,90]]}
-    target = {('image01','cat'):[[20,20,41,41]],('image01','dog'):[[60,60,91,91]],('image02','cat'):[[30,30,51,51]]}
-    voc_eval(preds,target,VOC_CLASSES=['cat','dog'])
+    preds = {
+        'cat': [
+            ['image01', 0.9, 20, 20, 40, 40],
+            ['image01', 0.8, 20, 20, 50, 50],
+            ['image02', 0.8, 30, 30, 50, 50]
+        ],
+        'dog': [
+            ['image01', 0.78, 60, 60, 90, 90]
+        ]
+    }
+
+    target = {
+        ('image01', 'cat'): [[20, 20, 41, 41]],
+        ('image01', 'dog'): [[60, 60, 91, 91]],
+        ('image02', 'cat'): [[30, 30, 51, 51]]
+    }
+    voc_eval(preds, target, VOC_CLASSES=['cat', 'dog'])
 
 
 if __name__ == '__main__':
-    #test_eval()
+    # test_eval()
     from predict import *
     from collections import defaultdict
     from tqdm import tqdm
 
-    target =  defaultdict(list)
+    target = defaultdict(list)
     preds = defaultdict(list)
-    image_list = [] #image path list
+    image_list = []  # image path list
 
     f = open('datasets/voc2007test.txt')
     lines = f.readlines()
@@ -165,9 +179,8 @@ if __name__ == '__main__':
         file_list.append(splited)
     f.close()
     print('---prepare target---')
-    for index,image_file in enumerate(file_list):
+    for index, image_file in enumerate(file_list):
         image_id = image_file[0]
-
         image_list.append(image_id)
         num_obj = (len(image_file) - 1) // 5
         for i in range(num_obj):
@@ -177,30 +190,23 @@ if __name__ == '__main__':
             y2 = int(image_file[4+5*i])
             c = int(image_file[5+5*i])
             class_name = VOC_CLASSES[c]
-            target[(image_id,class_name)].append([x1,y1,x2,y2])
+            target[(image_id, class_name)].append([x1, y1, x2, y2])
     #
-    #start test
+    # start test
     #
     print('---start test---')
     # model = vgg16_bn(pretrained=False)
     model = resnet50()
-    # model.classifier = nn.Sequential(
-    #             nn.Linear(512 * 7 * 7, 4096),
-    #             nn.ReLU(True),
-    #             nn.Dropout(),
-    #             #nn.Linear(4096, 4096),
-    #             #nn.ReLU(True),
-    #             #nn.Dropout(),
-    #             nn.Linear(4096, 1470),
-    #         )
     model.load_state_dict(torch.load('checkpoints/best.pth'))
     model.eval()
     model.cuda()
     count = 0
     for image_path in tqdm(image_list):
-        result = predict_gpu(model,image_path,root_path='datasets/images/') #result[[left_up,right_bottom,class_name,image_path],]
-        for (x1,y1),(x2,y2),class_name,image_id,prob in result: #image_id is actually image_path
-            preds[class_name].append([image_id,prob,x1,y1,x2,y2])
+        # result[[left_up,right_bottom,class_name,image_path],]
+        result = predict_gpu(model, image_path, root_path='datasets/images/')
+        # image_id is actually image_path
+        for (x1, y1), (x2, y2), class_name, image_id, prob in result:
+            preds[class_name].append([image_id, prob, x1, y1, x2, y2])
         # print(image_path)
         # image = cv2.imread('/home/xzh/data/VOCdevkit/VOC2012/allimgs/'+image_path)
         # for left_up,right_bottom,class_name,_,prob in result:
@@ -218,4 +224,4 @@ if __name__ == '__main__':
         #     break
     
     print('---start evaluate---')
-    voc_eval(preds,target,VOC_CLASSES=VOC_CLASSES)
+    voc_eval(preds, target, VOC_CLASSES=VOC_CLASSES)
